916386111f1be24a35446a10a65e801c85d59a4d
Lucian Julio <lucian.julio@ccc.ufcg.edu.br>
2019-09-24 09:58:25 -0300

    Alterei estadia

--- src/hotel/Estadia.java
+++ src/hotel/Estadia.java
@@ -1,6 +1,7 @@
 package hotel;
 
 public class Estadia {
+	
 	private Animal animalHospedado;
 	private double valor;
 	private int dias;
@@ -9,38 +10,29 @@ public class Estadia {
 		
 	}
 	
-	
-
 	@Override
 	public String toString() {
 		return this.animalHospedado.getNome()+","+this.animalHospedado.getTipo()+","+this.dias+" dias com o preço de R$ "+this.valor;
 	}
 
-
-
-	public Animal getAnimalHospedado() {
-		return animalHospedado;
-	}
-
-	public void setAnimalHospedado(Animal animalHospedado) {
-		this.animalHospedado = animalHospedado;
+	public String getNome() {
+		return this.animalHospedado.getNome();
 	}
 
 	public double getValor() {
 		return valor;
 	}
 
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
-
 	public int getDias() {
 		return dias;
 	}
-
-	public void setDias(int dias) {
-		this.dias = dias;
-	}
 	
+	public int getIdade() {
+		return this.animalHospedado.getIdade();
+	}
+	public String getTipo() {
+		return this.animalHospedado.getTipo();
+	}
+
 	
 }
