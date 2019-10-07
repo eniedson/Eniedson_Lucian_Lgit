161db5a7c815e4cbe70137325a39d8e3ef12354b
reginaLeticia <regina.felipe@ccc.ufcg.edu.br>
2019-10-07 09:02:18 -0300

    Documentações

--- src/hotel/Recepcao.java
+++ src/hotel/Recepcao.java
@@ -5,18 +5,29 @@ import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
 import java.util.Locale;
 
+/**
+ * 
+ * Representação de uma recepção de um hotel, possui um coleção de estadias e
+ * funcionalidades de um hotel
+ *
+ * 
+ * 
+ */
 public class Recepcao {
-	
+
+	/**
+	* Coleção de estadias
+	*/
 	private ArrayList<Estadia> estadias;
-	
+
 	public Recepcao() {
 		estadias = new ArrayList<Estadia>();
 	}
-	
+
 	public void checkIn(String nome, String tipo, int idade, int dias, double valor) {
 		estadias.add(new Estadia(nome, tipo, idade, dias, valor));
 	}
-	
+
 	public void checkOut(String nome) {
 		for (Estadia estadia : estadias) {
 			if (estadia.getNome().equals(nome)) {
@@ -25,11 +36,11 @@ public class Recepcao {
 			}
 		}
 	}
-	
+
 	public int getNumDeHospedes() {
 		return estadias.size();
 	}
-	
+
 	public double getLucroTotal() {
 		double lucro = 0;
 		for (Estadia estadia : estadias) {
@@ -37,12 +48,13 @@ public class Recepcao {
 		}
 		return lucro;
 	}
-	
+
 	public String toString() {
 		String saida = "Estadias:";
 		DecimalFormat formater = new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.ENGLISH));
 		for (Estadia estadia : estadias) {
-			saida += "\n" + estadia.getNome() + " (" + estadia.getTipo() + "): " + estadia.getDias() + " dias com o preco de R$ " + formater.format(estadia.getValor());
+			saida += "\n" + estadia.getNome() + " (" + estadia.getTipo() + "): " + estadia.getDias()
+					+ " dias com o preco de R$ " + formater.format(estadia.getValor());
 		}
 		return saida;
 	}
